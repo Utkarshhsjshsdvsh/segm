21e4deacf83c0a9e3ce6c5983e0e5f034e6e7cef
utkarsh <132475735+Utkarshhsjshsdvsh@users.noreply.github.com>
2024-03-11 20:03:47 +0530

    Update goku.cpp

--- goku.cpp
+++ goku.cpp
@@ -66,3 +66,96 @@ int main() {
 	solve();
 	return 0;
 }
+#include <bits/stdc++.h>
+using namespace std;
+class info{
+    public:
+    int full;
+    int open;
+    int close;
+    info()
+    {
+        full=0;
+        open=0;
+        close=0;
+    }
+};
+void merge(info seg1,info seg2,info &seg3)
+{
+    info ans;
+    ans.full=seg1.full+seg2.full+min(seg1.open,seg2.close);
+    ans.open=seg1.open+seg2.open-min(seg1.open,seg2.close);
+    ans.close=seg1.close+seg2.close-min(seg1.open,seg2.close);
+    seg3 = ans;
+}
+void build(int ind,int lo,int hi,string &s,info seg[])
+{
+    if(hi==lo){
+        if(s[lo]=='(')
+        seg[ind].open++;
+        else
+        seg[ind].close++;
+        return ;
+    }
+    int mid=(hi+lo)/2;
+    build(2*ind+1,lo,mid,s,seg);
+    build(2*ind+2,mid+1,hi,s,seg);
+    info m;
+    merge(seg[2*ind+1],seg[2*ind+2],m);
+    seg[ind]=m;
+}
+info query(int ind,int lo,int hi,int l,int r,info seg[])
+{
+    if(hi<l || r<lo)
+    {
+        info m;
+        return m;
+    }
+    if(hi<=r && lo>=l)
+    return seg[ind];
+    int mid=(hi+lo)/2;
+    info left=query(2*ind+1,lo,mid,l,r,seg);
+    info right=query(2*ind+2,mid+1,hi,l,r,seg);
+    info m;
+    merge(left,right,m);
+    return m;
+}
+/*void update(int ind,int lo,int hi,int i,int val,vector<int>&seg,int orr)
+{
+    if(lo==hi)
+    {
+        seg[ind]=val;
+        return;
+    }
+    int mid=(hi+lo)/2;
+    if(i<=mid)
+    update(2*ind+1,lo,mid,i,val,seg,!orr);
+    else
+    update(2*ind+2,mid+1,hi,i,val,seg,!orr);
+    if(orr)
+    seg[ind]=seg[2*ind+1]|seg[2*ind+2];
+    else
+    seg[ind]=seg[2*ind+1]^seg[2*ind+2];
+}*/
+void solve()
+{
+    string s;
+    cin>>s;
+    int n;
+    cin>>n;
+    info seg[s.size()*4];
+    build(0,0,s.size()-1,s,seg);
+    for(int i=0;i<n;i++)
+    {
+        int l,r;
+        cin>>l>>r;
+        l--;
+        r--;
+        cout<<query(0,0,s.size()-1,l,r,seg).full*2<<endl;
+    }
+}
+int main() {
+	solve();
+	return 0;
+}
+
